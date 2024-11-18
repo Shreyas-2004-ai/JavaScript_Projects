cc7cee4a1509b648b1387784182ed8cb75742c1d
Shreyas-2004-ai <shreyasssanil62@gmail.com>
2024-11-18 16:17:51 +0530

    first commit

---
+++ Project1/dicegame.js
@@ -0,0 +1,15 @@
+function rolldice(){
+    const no_of_dice=document.getElementById("dicenum").value;
+    const dice_result=document.getElementById("diceresult");
+    const dice_images=document.getElementById("diceimages");
+    const values=[];
+    const images=[];
+
+    for(i=0;i<no_of_dice;i++){
+        const value=Math.floor(Math.random()*6)+1;
+        values.push(value)
+        images.push(`<img src="diceimages/${value}.png">`);
+    }
+    dice_result.textContent=`Dice Result:${values.join(", ")}`;
+    dice_images.innerHTML=images.join(" ");
+}
